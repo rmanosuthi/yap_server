95330d9d0220c1b55713ebbca930340cd319f17a
Ratchaphon Manosuthi <themelo64@gmail.com>
2021-01-08 04:18:55 +0700

    Example launch config

--- dev.sh
+++ dev.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
-
+# Example launch json:
+# {
+# 	"db_addr": "mysql://dev:dev@127.0.0.1:3306",
+# 	"api_addr": "127.0.0.1:8080",
+# 	"ws_addr": "127.0.0.1:9999"
+# }
 RUST_LOG=debug cargo run -- -c 'dev_launch.json'
